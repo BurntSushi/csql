1ce6d8831ad6a482a5657c952b39a15de704ed33
Andrew Gallant <jamslam@gmail.com>
2014-01-26 22:28:40 -0500

    Simplify the Inserter API.
    Add support for CopyIn when the driver is postgres.

--- insert.go
+++ insert.go
@@ -4,51 +4,79 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+
+	"github.com/lib/pq"
 )
 
-type PrepExecer interface {
-	Exec(query string, args ...interface{}) (sql.Result, error)
-	Prepare(query string) (*sql.Stmt, error)
-}
+// TODO: Modify this so it uses COPY IN from github.com/lib/pq when it's
+// available.
+//
+// But how do we know if the pq package is being used? Hmm...
 
 type Inserter struct {
-	db            PrepExecer
+	tx            *sql.Tx
 	current, size int
 	table         string
 	columns       []string
-	data          []interface{}
+
+	stmt *sql.Stmt
+	data []interface{}
 }
 
 func NewInserter(
-	db PrepExecer,
+	tx *sql.Tx,
+	driver string,
 	size int,
 	table string,
-	columns ...string) *Inserter {
+	columns ...string,
+) (*Inserter, error) {
 	if size < 1 {
 		size = 1
 	}
-	return &Inserter{db, 0, size, table, columns, make([]interface{}, 0, 100)}
+	var ins *Inserter
+	err := Safe(func() {
+		ins = &Inserter{tx, 0, size, table, columns, nil, nil}
+		if driver == "postgres" {
+			ins.stmt = Prepare(tx, pq.CopyIn(table, columns...))
+		} else {
+			ins.data = make([]interface{}, 0, 100)
+		}
+	})
+	return ins, err
 }
 
-func (in *Inserter) Add(args ...interface{}) error {
+func (in *Inserter) Exec(args ...interface{}) error {
+	if in.stmt != nil {
+		_, err := in.stmt.Exec(args...)
+		if err != nil {
+			return err
+		}
+		if len(args) == 0 {
+			return in.stmt.Close()
+		}
+		return nil
+	}
+	if len(args) == 0 {
+		return in.run()
+	}
 	if len(args) != len(in.columns) {
 		panic("len(args) != len(columns)")
 	}
 	in.data = append(in.data, args...)
 	in.current++
 	if in.current == in.size {
-		return in.Exec()
+		return in.run()
 	}
 	return nil
 }
 
-func (in *Inserter) Exec() error {
+func (in *Inserter) run() error {
 	err := Safe(func() {
 		q := in.prepare()
 		if len(q) == 0 {
 			return // nothing to insert! success!
 		}
-		Exec(in.db, q, in.data...)
+		Exec(in.tx, q, in.data...)
 	})
 	in.current = 0
 	in.data = in.data[:0]
@@ -70,7 +98,7 @@ func (in *Inserter) prepare() string {
 
 	rows := make([]string, in.current)
 	for i := range rows {
-		rows[i] = makePlaceholders(1 + i * len(in.columns))
+		rows[i] = makePlaceholders(1 + i*len(in.columns))
 	}
 	return fmt.Sprintf("INSERT INTO %s (%s) VALUES %s",
 		in.table, strings.Join(in.columns, ", "), strings.Join(rows, ", "))
